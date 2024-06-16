edc492cbea22e551ab57fb01632c795ebc8c5380
uezu <73319501+ArthurUezu@users.noreply.github.com>
2024-06-16 16:47:29 -0300

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -131,7 +131,7 @@ def main():
         if 'Messages' in response: 
             message = response['Messages'][0] 
             print('Received message:', message['Body'])    
-            execute(message)
+            execute(message['Body'])
             # Delete the received message 
             sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle']) 
         sleep(5)
